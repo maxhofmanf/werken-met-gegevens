4f3697fa7c5a5cb217e8acd6aafed796f2765e2c
maxhofmanf <99068281@mydavinci.nl>
2021-09-07 10:34:19 +0100

    werken-met-gegevens

---
+++ feestlunch.py
@@ -0,0 +1,4 @@
+croissantje = 0.39
+stokbroden = 2.78
+kortingsbonnen = 0.50
+print( croissantje * 17 + ( stokbroden * 2 - ( kortingsbonnen * 3)))
---
+++ speelhaldag.py
@@ -0,0 +1,5 @@
+toegangsticket = 7.45
+VijfMinuten = 0.37
+CompleetAantalMinuten = 45
+AantalPersonen = 4
+print( toegangsticket * AantalPersonen + ( 45/ 5 * (VijfMinuten) ))
