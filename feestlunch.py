d574280f4d68d0dbdc49c76695939db8cd6542cc
maxhofmanf <99068281@mydavinci.nl>
2021-09-08 09:00:47 +0100

    werken-met-gegevens

--- feestlunch.py
+++ feestlunch.py
@@ -1,4 +1,7 @@
-croissantje = 0.39
+Croissantje = 0.39
 stokbroden = 2.78
-kortingsbonnen = 0.50
-print( croissantje * 17 + ( stokbroden * 2 - ( kortingsbonnen * 3)))
+kortingsbonnen = 3
+Korting = 1.50
+
+print( Croissantje * 17 + stokbroden * 2 - ( 0.50 * 3))
+print('De feestlunch kost je bij de bakker 10.69 euro voor de 17 croissant en 2 stokbroden')
